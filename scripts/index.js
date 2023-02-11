a0672270c87d605ae42399609fc50c52335eb676
Sergeev Dmitry <sy1er@yandex.ru>
2023-02-12 00:50:31 +0400

    feat: Сделать отрисовку карт средствами js, удалени карт кнопкой

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,43 @@ let popupForm = popup.querySelector('.popup__form');
 let popupCloseBtn = popup.querySelector('.popup__close');
 let inputName = popup.querySelector('.popup__input_type_name');
 let inputActivity = popup.querySelector('.popup__input_type_activity');
+let photoList = document.querySelector('.photo__list');
+let cardTemplate =  document.querySelector('#cardTemplate').content;
+
+
+const initialCards = [
+  {
+    name: 'Берёзовая роща',
+    link: './images/birch.jpg'
+  },
+  {
+    name: 'Дуб великан',
+    link: './images/giant-oak.jpg'
+  },
+  {
+    name: 'Стражи леса',
+    link: './images/guardians.webp'
+  },
+  {
+    name: 'Тенистый пруд',
+    link: './images/shady-pond.jpg'
+  },
+  {
+    name: 'Серебрянный родник',
+    link: './images/silver-spring.jpg'
+  },
+  {
+    name: 'Местные жители',
+    link: './images/squirrel.jpg'
+  }
+];
+
+initialCards.forEach ((cardData) => {
+  const card = cardRendering(cardData.name,cardData.link);
+  photoList.append(card);
+ });
+ /* let deleteBtn = document.querySelectorAll('.photo__delete-btn'); */
+/*  console.log(deleteBtn +"dsadasd"); */
 
 function showPopup () {
   popup.classList.add('popup_opened');
@@ -29,6 +66,21 @@ function handleFormSubmit (evt) {
   }
 }
 
+function cardRendering (name,link) {
+  const card = cardTemplate.querySelector('.photo__card').cloneNode(true);
+  card.querySelector('.photo__name').textContent = name;
+  card.querySelector('.photo__image').alt = name;
+  card.querySelector('.photo__image').src= link;
+  return card;
+}
+
+function deleteCard (targetBnt) {
+  console.log(targetBnt);
+  const card = targetBnt.target.closest('.photo__card');
+  console.log(targetBnt);
+  card.remove();
+}
+photoList.addEventListener('click', deleteCard);
 editBtn.addEventListener('click', showPopup);
 popupCloseBtn.addEventListener('click', hidePopup);
 popupForm.addEventListener('submit', handleFormSubmit);
