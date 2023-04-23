d3b5beaace555e537e95697207cce2925b7c24cd
Sergeev Dmitry <sy1er@yandex.ru>
2023-04-23 16:04:58 +0400

    refactor: исправить README

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
         use: 'babel-loader',
         exclude: /node_modules/
       },
-      {
+       {
         test: /\.(webp|png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
         type: 'asset/resource',
       },
